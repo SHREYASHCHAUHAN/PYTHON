ac4896c1ac26984246aa7e1754632903316fa7d3
SHREYASHCHAUHAN <97503564+SHREYASHCHAUHAN@users.noreply.github.com>
2022-01-13 07:36:12 +0530

    Delete SET-D.py

--- SET-D.py
+++
@@ -1,6 +0,0 @@
-print("E. Write a Python program to find the most common elements and their counts from list, tuple, and dictionary.")
-print("D21CE180 - SHREYASH CHAUHAN")
-from collections import Counter
-set = 'MY NAME IS SHREYASH CHAUHAN'
-print(set)
-print("MOST COMMON CHARACTERS OF THE SET :",Counter(set).most_common(5))
