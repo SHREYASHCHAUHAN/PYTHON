defca8eee8a860d559a6a8e3d4740b06134eb46f
SHREYASHCHAUHAN <97503564+SHREYASHCHAUHAN@users.noreply.github.com>
2022-01-13 07:48:55 +0530

    Add files via upload

---
+++ SET-C.py
@@ -0,0 +1,8 @@
+print("C. Write a Python program to create an intersection, Union, difference of sets.")
+print("D21CE180 - SHREYASH CHAUHAN")
+A = {1, 2, 3, 4, 5, 6}
+B = {2, 4, 5, 6, 7, 8}
+print("UNION OF A & B :", A | B)
+print("INTERSECTION OF A & B :", A & B)
+print("DIFFERENCE OF A & B :", A - B)
+print("SYMMETRIC DIFFERENCE OF A & B :", A ^ B)
