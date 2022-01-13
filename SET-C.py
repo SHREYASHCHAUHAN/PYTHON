bb4fc3482a2c201e91b2c0a8b2ae4007b58009ea
SHREYASHCHAUHAN <97503564+SHREYASHCHAUHAN@users.noreply.github.com>
2022-01-13 07:36:01 +0530

    Delete SET-C.py

--- SET-C.py
+++
@@ -1,6 +0,0 @@
-print("D. Write a Python program to find maximum and the minimum value in a set.")
-print("D21CE180 - SHREYASH CHAUHAN")
-A = {1, 20, 3, 4, 50, 600}
-print("ALL ELEMENTS OF SET :", A)
-print("MAXIMUM VALUE OF THE SET :", max(A))
-print("MINIMUM VALUE OF THE SET :", min(A))
