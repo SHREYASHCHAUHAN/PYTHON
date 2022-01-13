18cc6c9149f1a590d33c2a1ae28a4eb4de34fac6
SHREYASHCHAUHAN <97503564+SHREYASHCHAUHAN@users.noreply.github.com>
2022-01-13 07:18:11 +0530

    Add files via upload

---
+++ DICTIONARY-A.py
@@ -0,0 +1,9 @@
+print("b. Write a Python script to merge two Python dictionaries")
+print("D21CE180 - SHREYASH CHAUHAN")
+d1 = {'a': 100, 'b': 200}
+d2 = {'c': 300, 'd': 400}
+d = d1.copy()
+d.update(d2)
+print("FIRST DICTIONARY :", d1)
+print("SECOND DICTIONARY :", d2)
+print("AFTER MERGE :", d)
---
+++ DICTIONARY-B.py
@@ -0,0 +1,6 @@
+print("C. Write a Python program to sum all the items in a dictionary.")
+print("D21CE180 - SHREYASH CHAUHAN")
+d1 = {'a': 100, 'b': 200, 'c': 300, 'd': 400}
+d2 = sum(d1.values())
+print("DICTIONARY :", d1)
+print("SUM OF ALL VALUES IN DICTIONARY :", d2)
