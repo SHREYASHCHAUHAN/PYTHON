fb9c3512aa9fe7725b23b069280daf7ce65eeeee
SHREYASHCHAUHAN <97503564+SHREYASHCHAUHAN@users.noreply.github.com>
2022-01-13 07:46:27 +0530

    Add files via upload

---
+++ TUPLE-C.py
@@ -0,0 +1,8 @@
+print("C. Write a Python program to add an item in a tuple.")
+print("D21CE180 - SHREYASH CHAUHAN")
+t = (1, 2, 3, 4, 5)
+print("BEFORE ADDING ITEM =", t)
+v = list(t)
+v.append(6)
+t = tuple(v)
+print("AFTER ADDING ITEM =", t)
