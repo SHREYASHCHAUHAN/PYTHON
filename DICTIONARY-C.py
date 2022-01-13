3c21e8e0969099d7950686c90e68e0c3c8fdb368
SHREYASHCHAUHAN <97503564+SHREYASHCHAUHAN@users.noreply.github.com>
2022-01-13 07:35:15 +0530

    Delete DICTIONARY-C.py

--- DICTIONARY-C.py
+++
@@ -1,9 +0,0 @@
-print("D. Write a Python script to add a key to a dictionary.")
-print("D21CE180 - SHREYASH CHAUHAN")
-d1 = {0: 10, 1: 20}
-d2 = {2: 30}
-d = d1.copy()
-d.update(d2)
-print("DICTIONARY :", d1)
-print("KEY TO ADD IN DICTIONARY :", d2)
-print("FINAL RESULT :", d)
